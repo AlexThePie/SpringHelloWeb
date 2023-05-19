37939d1eb1059cbdc3e7f6916a86e06b0367666b
AlexThePie <112374973+AlexThePie@users.noreply.github.com>
2023-05-19 16:26:48 +0400

    Initial commit

---
+++ src/test/java/ru/diasoft/alexrusskin/springhelloweb/SpringHelloWebApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.diasoft.alexrusskin.springhelloweb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringHelloWebApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
